62fe81d6c96cffaa0d1b1dc7c5569b18c1714e66
Lukas Lechner <lukas.k.lechner@gmail.com>
2018-06-14 18:26:24 +0200

    updated fisher plots, overkill but flexible

--- plots/plotsLukas/fisher_information/fisher_information_ttZ.py
+++ plots/plotsLukas/fisher_information/fisher_information_ttZ.py
@@ -25,6 +25,9 @@ from TTXPheno.samples.benchmarks         import *
 # Import helpers
 from plot_helpers                        import *
 
+# Import additional
+from array                               import array
+
 # Arguments
 import argparse
 argParser = argparse.ArgumentParser(description = "Argument parser")
@@ -84,116 +87,140 @@ weightString = 'ref_lumiweight1fb*%s*%s' %( str(args.luminosity), str(event_fact
 weightFunction = get_reweight_function()
 
 # split selection string step by step
-selectionStrings = []
+selections = []
 selectionElements = args.selection.split('-')
 for i, item in enumerate(selectionElements[:-1]):
-    selectionStrings.append( {'plotstring':' + '.join(replace_selectionstrings(selectionElements[:i+1])) + ' selection', 'selection':'-'.join(selectionElements[:i+1])} )
-selectionStrings.append( {'plotstring':'full pre-selection', 'selection':args.selection} )
+    selections.append( {'plotstring':' + '.join(replace_selectionstrings(selectionElements[:i+1])) + ' selection', 'selection':'-'.join(selectionElements[:i+1])} )
+
+selections.append( {'plotstring':'full pre-selection', 'selection':args.selection} )
+    
 
 # Additional plot variables
-plot_variables =   [
-                     { 'plotstring':'p_{T}(Z)',            'var':'Z_pt',           'binning':[ 20000, 0, 500 ] },
-                     { 'plotstring':'p_{T}(Z)',            'var':'Z_pt',           'binning':[ 2000, 0, 500 ] },
-                     { 'plotstring':'p_{T}(Z)',            'var':'Z_pt',           'binning':[ 200, 0, 500 ] },
+plotVariables =   [
+                     { 'plotstring':'p_{T}(Z)',            'var':'Z_pt',           'binning':[ 100000, 0, 1000 ] },
+                     { 'plotstring':'p_{T}(Z)',            'var':'Z_pt',           'binning':[ 20000, 0, 1000 ] },
+                     { 'plotstring':'p_{T}(Z)',            'var':'Z_pt',           'binning':[ 2000, 0, 1000 ] },
+                     { 'plotstring':'p_{T}(Z)',            'var':'Z_pt',           'binning':[ 200, 0, 1000 ] },
                      { 'plotstring':'p_{T}(Z)',            'var':'Z_pt',           'binning':[ 20, 0, 1000 ] },
                      { 'plotstring':'cos(#theta*)',        'var':'Z_cosThetaStar', 'binning':[ 20, -1.2, 1.2 ] },
                      { 'plotstring':'m_{ll}',              'var':'Z_mass',         'binning':[ 20, 70, 110 ] },
                      { 'plotstring':'#phi(Z)',             'var':'Z_phi',          'binning':[ 20, -np.pi, np.pi ] },
                      { 'plotstring':'#eta(Z)',             'var':'Z_eta',          'binning':[ 20, -3, 3 ] },
-                     { 'plotstring':'p_{T}(E_{T}^{miss})', 'var':'GenMet_pt',      'binning':[ 20, 0, 500 ] },
+                     { 'plotstring':'p_{T}(E_{T}^{miss})', 'var':'GenMet_pt',      'binning':[ 20, 0, 1000 ] },
                      { 'plotstring':'#phi(E_{T}^{miss})',  'var':'GenMet_phi',     'binning':[ 20, -np.pi, np.pi ] },
 ]
 
+
 # Calculate coefficients for binned distribution
 # Calculate determinant using the 'variables' submatrix of FI
-for var in plot_variables:
+for var in plotVariables:
     var['coeff']       = getCoeffPlotFromDraw( sample, args.order, var['var'], var['binning'], selection_string, weightString=weightString )
-    var['detI']        = np.linalg.det( w.get_total_fisherInformation_matrix( var['coeff'], args.variables, **WC )[1] )
     # add bin information to plot labels
     var['plotstring'] += ' (' + str(var['binning'][0]) + ' bins)'
-
-# sort by Fisher Information
-#plot_variables = sorted(plot_variables, key=lambda k: -k['detI'])
+    var['color']       = 30
 
 # Calculate coefficients unbinned (event loop)
 # Calculate determinant using the 'variables' submatrix of FI
-for selection in selectionStrings:
+for selection in selections:
     selection['coeff'] = getCoeffListFromEvents( sample, selectionString = cutInterpreter.cutString(selection['selection']), weightFunction = weightFunction )
-    selection['detI']  = np.linalg.det( w.get_total_fisherInformation_matrix( selection['coeff'], args.variables, **WC )[1] )
-
-# sort by Fisher Information
-#selectionStrings = sorted(selectionStrings, key=lambda k: -k['detI'])
+    selection['color'] = 46
 
 # Full Fisher information
-full          = { 'plotstring':'full'}
-full['coeff'] = getCoeffListFromEvents( sample, selectionString = None, weightFunction = weightFunction )
-full['detI']  = np.linalg.det( w.get_total_fisherInformation_matrix( full['coeff'], args.variables, **WC )[1] )
+full              = { 'plotstring':'full'}
+full['coeff']     = getCoeffListFromEvents( sample, selectionString = None, weightFunction = weightFunction )
+full['color']     = 15
 
-
-# Data for plotting
-data = [full] + selectionStrings + plot_variables
 expo = 1. / len(args.variables)
-detI = [ abs( item['detI'] / full['detI'] )**expo if full['detI'] != 0 else 0 for item in data ]
-n = len(detI)
+data = [full] + selections + plotVariables
+n_data = len(data)
 
-print(detI)
+# Fill dictionaries with normalized fisher information and plotting data
+for i,item in enumerate(data):
+    item['detI']      = np.linalg.det( w.get_total_fisherInformation_matrix( item['coeff'], args.variables, **WC )[1] )
+    item['norm_detI'] = abs( item['detI'] / data[0]['detI'] )**expo if data[0]['detI'] != 0 else 0
+    item['x_graph']   = array( 'd', range( 1, n_data+1 ) )
+    item['y_graph']   = array( 'd', [0]*i + [item['norm_detI']] + [0]*(n_data-i) )
 
-# Prepare data for plotting
-from array import array
-x = array('d')
-y = array('d')
 
-for i in range(n):
-    x.append(i+1)
-    y.append( detI[i] )
+# Plots
+def drawPlot( log = False ): 
+    ''' Plotting function
+    '''
 
-# Plotting
-c1 = ROOT.TCanvas()
-c1.SetBottomMargin(0.05)
+    def getTGraph( n, x, y, color=40 ):
+        ''' Create a TGraph object
+        '''
 
-# Plot
-gr = ROOT.TGraph( n, x, y )
-gr.GetXaxis().SetTickLength(0.)
-gr.SetFillColor( 40 )
-gr.SetLineWidth( 4 )
-gr.GetXaxis().SetLabelSize(0)
-gr.GetXaxis().SetLabelOffset(999)
-gr.GetYaxis().SetTitle( '(det(I_{ij}) / det(I_{ij}^{full}))^{(1/%s)}'%len(args.variables) if len(args.variables)>1 else 'det(I_{ij}) / det(I_{ij}^{full})' )
-gr.GetYaxis().SetLabelSize(0.035)
-gr.GetYaxis().SetTitleSize(0.035)
-gr.GetYaxis().SetRangeUser(0., 1.1)
-gr.Draw( 'AB' )
-c1.Update()
+        gr = ROOT.TGraph( n, x, y )
+        gr.SetFillColor( color )
+        gr.SetLineWidth( 4 )
 
-# Info
-t1 = ROOT.TPaveText(0.55, .85, .95, .92, "blNDC")
-t1.SetTextAlign(33)
-t1.SetTextSize(0.035)
-t1.SetTextFont(1)
-t1.SetBorderSize(0)
-t1.SetFillColor(ROOT.kWhite)
-t1.AddText('Restricted to: ' + ', '.join(args.variables))
-t1.AddText('WC: ' + ' '.join(args.parameters))
-t1.Draw()
+        return gr
 
-# Selection info
-t = ROOT.TLatex()
-t.SetTextAngle(90)
-t.SetTextSize(0.035)
-labels = [ item['plotstring'] for item in data ] 
 
-for i, label in enumerate(labels):
-    t.DrawLatex(i+1.25, 0.05, label);
+    # Canvas
+    c1 = ROOT.TCanvas()
+    c1.SetBottomMargin(0.05)
 
-# Directory
-plot_directory_ = os.path.join(\
-    plot_directory,
-    args.plot_directory, 
-    sample.name, 
-    fisher_directory, 
-    args.selection,
-    WC_string)
+    mg = ROOT.TMultiGraph()
 
-if not os.path.isdir(plot_directory_): os.makedirs(plot_directory_)
-c1.Print(os.path.join(plot_directory_, 'fisher_information.png'))
+    # Add a TGraph for each 'bin' (overkill but flexible in changing the color)
+    for item in data:
+        mg.Add( getTGraph( n_data, item['x_graph'], item['y_graph'], item['color'] ) )
 
+    mg.Draw('AB')
+
+    # Layout
+    mg.GetXaxis().SetLabelSize(0)
+    mg.GetXaxis().SetTickLength(0.)
+    mg.GetXaxis().SetLabelOffset(999)
+    mg.GetYaxis().SetTitle( '(det(I_{ij}) / det(I_{ij}^{full}))^{(1/%s)}'%len(args.variables) if len(args.variables)>1 else 'det(I_{ij}) / det(I_{ij}^{full})' )
+    mg.GetYaxis().SetLabelSize(0.035)
+    mg.GetYaxis().SetTitleSize(0.035)
+    mg.GetYaxis().SetRangeUser(0., 1.1)
+
+    if log:
+        mg.GetYaxis().SetRangeUser(.001, 2.)
+        c1.SetLogy()
+
+    c1.Update()
+
+    # Info
+    t1 = ROOT.TPaveText(0.55, .85, .95, .92, "blNDC")
+    t1.SetTextAlign(33)
+    t1.SetTextSize(0.035)
+    t1.SetTextFont(1)
+    t1.SetBorderSize(0)
+    t1.SetFillColor(ROOT.kWhite)
+    t1.AddText('Restricted to: ' + ', '.join(args.variables))
+    t1.AddText('WC: ' + ' '.join(args.parameters))
+    t1.Draw()
+    
+    # Selection info
+    t = ROOT.TLatex()
+    t.SetTextAngle(90)
+    t.SetTextSize(0.035)
+
+    # Labeling
+    for i, item in enumerate(data):
+        t.DrawLatex( i+1.25, 0.0014 if log else 0.05, item['plotstring'] )
+    
+    # Directory
+    plot_directory_ = os.path.join(\
+        plot_directory,
+        args.plot_directory, 
+        sample.name, 
+        fisher_directory, 
+        args.selection,
+        WC_string,
+        'log' if log else 'lin')
+    
+    if not os.path.isdir(plot_directory_): os.makedirs(plot_directory_)
+    c1.Print(os.path.join(plot_directory_, 'fisher_information.png'))
+    
+    del c1
+
+
+# Plot lin and log plots
+for log in [ True, False ]:
+    drawPlot( log )
