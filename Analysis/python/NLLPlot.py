c3c08161438bf89f7b79cd2b1d546152372b44e5
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2018-07-09 08:34:23 +0200

    NLL and exclusion plot

--- Analysis/python/NLLPlot.py
+++ Analysis/python/NLLPlot.py
@@ -27,9 +27,9 @@ from TTXPheno.Tools.user import plot_directory
 ttZ_sample = fwlite_ttZ_ll_LO_order2_15weights_ref
 
 # reduce dataset
-#ttZ_sample.reduceFiles( to = 1 )
+ttZ_sample.reduceFiles( to = 5 )
 # approximately compensate reduction of files
-#ttZ_sample.setWeightString("200")
+ttZ_sample.setWeightString("40")
 
 # get the reweighting function
 from TTXPheno.Tools.WeightInfo import WeightInfo
@@ -66,8 +66,8 @@ observation                  = {}
 signal_jec_uncertainty       = {}
 signal_fakerate_uncertainty  = {}
 ttZ_SM_rate                  = {}
-ttZ_SM_jec_uncertainty       = {}
-ttZ_SM_fakerate_uncertainty  = {}
+#ttZ_SM_jec_uncertainty       = {}
+#ttZ_SM_fakerate_uncertainty  = {}
 background_rate                 = {}
 background_jec_uncertainty      = {}
 background_fakerate_uncertainty = {}
@@ -79,10 +79,10 @@ for i_region, region in enumerate(regions):
 
     # compute signal yield for this region (this is the final code)
     ttZ_coeffList[region] = ttZ_sample.weightInfo.getCoeffListFromDraw( ttZ_sample, region.cutString(), weightString='150*ref_lumiweight1fb' ) 
-    # TTZ SM
+    ## TTZ SM
     ttZ_SM_rate[region] = ttZ_sample.weightInfo.get_weight_yield( ttZ_coeffList[region] )
-    ttZ_SM_jec_uncertainty      [region] = 1.05 
-    ttZ_SM_fakerate_uncertainty [region] = 1.0  # signal has no FR uncertainty
+    #ttZ_SM_jec_uncertainty      [region] = 1.05 
+    #ttZ_SM_fakerate_uncertainty [region] = 1.0  # signal has no FR uncertainty
 
     # signal uncertainties
     signal_jec_uncertainty      [region] = 1.05 
@@ -111,10 +111,11 @@ c = cardFileWriter.cardFileWriter()
 from TTXPheno.Analysis.ProfiledLoglikelihoodFit import ProfiledLoglikelihoodFit
 
 #binningX = binningY = [12, -12, 12]
+
 binningX = [24, -24-6, 24-6]
 binningY = [24, -24+16, 24+16]
 
-limit = ROOT.TH2F( 'limit', 'limit', *(binningX + binningY) )
+nll_plot = ROOT.TH2F( 'nll_plot', 'nll_plot', *(binningX + binningY) )
 
 for cpt in range( binningX[1], binningX[2], ( binningX[2] - binningX[1]) / binningX[0] ):
     for cpQM in range( binningY[1], binningY[2], ( binningY[2] - binningY[1]) / binningY[0] ):
@@ -128,14 +129,13 @@ for cpt in range( binningX[1], binningX[2], ( binningX[2] - binningX[1]) / binni
         c.addUncertainty('JEC',         'lnN')
         c.addUncertainty('fake',        'lnN')
 
-        #processes = [ "signal", "ttZ_SM_yield" ] + [s.name for s in backgrounds]
         signal_rate                  = {}
         for i_region, region in enumerate(regions):
-            signal_rate[region] = ttZ_sample.weightInfo.get_weight_yield( ttZ_coeffList[region], **kwargs) - ttZ_SM_rate[region] 
+            signal_rate[region] = ttZ_sample.weightInfo.get_weight_yield( ttZ_coeffList[region], **kwargs) 
 
             bin_name = "Region_%i" % i_region
             nice_name = region.__str__()
-            c.addBin(bin_name,['ttZ_SM'] + [s.name for s in backgrounds], nice_name)
+            c.addBin(bin_name,[s.name for s in backgrounds], nice_name)
             c.specifyObservation( bin_name, observation[region] )
 
             c.specifyFlatUncertainty( 'lumi', 1.05 )
@@ -144,43 +144,32 @@ for cpt in range( binningX[1], binningX[2], ( binningX[2] - binningX[1]) / binni
             c.specifyUncertainty( 'JEC', bin_name, 'signal', signal_jec_uncertainty[region])
             c.specifyUncertainty( 'fake',bin_name, 'signal', signal_fakerate_uncertainty[region])
 
-            c.specifyExpectation( bin_name, 'ttZ_SM', ttZ_SM_rate[region] )
-            c.specifyUncertainty( 'JEC', bin_name, 'ttZ_SM', ttZ_SM_jec_uncertainty[region])
-            c.specifyUncertainty( 'fake',bin_name, 'ttZ_SM', ttZ_SM_fakerate_uncertainty[region])
+            #c.specifyExpectation( bin_name, 'ttZ_SM', ttZ_SM_rate[region] )
+            #c.specifyUncertainty( 'JEC', bin_name, 'ttZ_SM', ttZ_SM_jec_uncertainty[region])
+            #c.specifyUncertainty( 'fake',bin_name, 'ttZ_SM', ttZ_SM_fakerate_uncertainty[region])
 
             for background in backgrounds:
                 c.specifyExpectation( bin_name, background.name, background_rate[region][background.name] )
                 c.specifyUncertainty( 'JEC', bin_name, background.name, background_jec_uncertainty[region][background.name])
                 c.specifyUncertainty( 'fake',bin_name, background.name, background_fakerate_uncertainty[region][background.name])
                 
-        c.writeToFile( './tmp_limit_card.txt' ) 
+        c.writeToFile( './tmp_nll_card.txt' ) 
 
-        # try to adjust rmax with some margin
-        exp_tot_sigmas = 0
-        max_rmax = float('inf')
-        for region in regions:
-            tot_background = sum( [ background_rate[region][background.name] for background in backgrounds ] )
-            exp_tot_sigmas += abs(signal_rate[region])/sqrt( tot_background )
-            # avoid total neg. yield
-            if signal_rate[region]<0: 
-                max_r = -tot_background/signal_rate[region]
-                if max_r<max_rmax:
-                    max_rmax = max_r
-            
-        rmax_est = 400./exp_tot_sigmas if exp_tot_sigmas>0 else 1.
-        if max_rmax < rmax_est:
-            rmax_est = 0.9*max_rmax # safety margin such that at least +10% total yield survives in the smallest SR
-
-        profiledLoglikelihoodFit = ProfiledLoglikelihoodFit( './tmp_limit_card.txt' )
-        profiledLoglikelihoodFit.make_workspace(rmin=0, rmax=rmax_est)
+        profiledLoglikelihoodFit = ProfiledLoglikelihoodFit( './tmp_nll_card.txt' )
+        profiledLoglikelihoodFit.make_workspace(rmin=0, rmax=1)
         #expected_limit = profiledLoglikelihoodFit.calculate_limit( calculator = "frequentist" )
-        expected_limit = profiledLoglikelihoodFit.calculate_limit( calculator = "asymptotic" )
-        logger.info( "Expected Limit: %f", expected_limit[0] )
-        limit.SetBinContent( limit.FindBin(cpt, cpQM), expected_limit[0] )
+        nll = profiledLoglikelihoodFit.likelihoodTest()
+        logger.info( "NLL: %f", nll)
+        nll_plot.SetBinContent( nll_plot.FindBin(cpt, cpQM), nll )
         profiledLoglikelihoodFit.cleanup()
 
-limitPlot = Plot2D.fromHisto( "2D_limit_3", texX = "cpt", texY = "cpQM", histos = [[limit]])
-limitPlot.drawOption = "colz"
+nll_SM = nll_plot.GetBinContent( nll_plot.FindBin(0,0))
+for bin_x in range(nll_plot.GetNbinsX()+1):
+    for bin_y in range(nll_plot.GetNbinsY()+1):
+        nll_plot.SetBinContent(bin_x, bin_y, nll_plot.GetBinContent(  nll_plot.GetBin(bin_x, bin_y) ) - nll_SM)
+
+nllPlot = Plot2D.fromHisto( "2D_nll_2", texX = "cpt", texY = "cpQM", histos = [[nll_plot]])
+nllPlot.drawOption = "colz"
 ROOT.gStyle.SetPaintTextFormat("2.2f")        
-plotting.draw2D( limitPlot, plot_directory = os.path.join( plot_directory, 'limits', ttZ_sample.name), extensions = ["png"])
+plotting.draw2D( nllPlot, plot_directory = os.path.join( plot_directory, 'nll', ttZ_sample.name), extensions = ["png"])
 
