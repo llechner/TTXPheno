80f033ca3862eebb894c8684d60f2ca4e6e025d5
Lukas Lechner <lukas.k.lechner@gmail.com>
2018-10-18 14:01:57 +0200

    change eta cuts for phase2

--- Tools/python/objectSelection.py
+++ Tools/python/objectSelection.py
@@ -11,7 +11,7 @@ def genJetId( fwlite_jet ):
 def isGoodGenJet( j ):
     ''' jet object selection
     '''
-    return j['pt'] > 30 and abs( j['eta'] ) < 2.4
+    return j['pt'] > 30 and abs( j['eta'] ) < 4.0 #2.4
 
 def isGoodGenPhoton( j ):
     ''' jet object selection
@@ -21,19 +21,19 @@ def isGoodGenPhoton( j ):
 def isGoodGenLepton( l ):
     ''' lepton object selection
     '''
-    return l['pt'] > 10 and abs( l['eta'] ) < 2.5 and abs( int(l['pdgId']) ) in [11,13]
+    return l['pt'] > 10 and abs( l['eta'] ) < 3.0 and abs( int(l['pdgId']) ) in [11,13] #eta < 2.5
 
 def isGoodRecoLepton( l ):
-    return l['pt'] > 10 and abs( l['eta'] ) < 2.5 and abs( int(l['pdgId']) ) in [11,13]
+    return l['pt'] > 10 and abs( l['eta'] ) < 3.0 and abs( int(l['pdgId']) ) in [11,13] #eta < 2.5
 
 def isGoodRecoMuon( l ):
-    return abs( l['pdgId'] ) == 13 and abs( l['eta'] ) < 2.5 and l['pt'] > 10
+    return abs( l['pdgId'] ) == 13 and abs( l['eta'] ) < 3.0 and l['pt'] > 10 #eta < 2.5
 
 def isGoodRecoElectron( l ):
-    return abs( l['pdgId'] ) == 11 and abs( l['eta'] ) < 2.5 and l['pt'] > 10
+    return abs( l['pdgId'] ) == 11 and abs( l['eta'] ) < 3.0 and l['pt'] > 10 #eta < 2.5
 
 def isGoodRecoJet( j, pt_var = 'pt'):
-    return  abs( j['eta'] ) < 2.4 and j[pt_var] > 30 and j['nCharged']>1 and j['nNeutrals']>0
+    return  abs( j['eta'] ) < 4.0 and j[pt_var] > 30 and j['nCharged']>1 and j['nNeutrals']>0 #eta < 2.4
 
 def isGoodRecoPhoton( g ):
     return  abs( g['eta'] ) < 2.1 and g['pt'] > 15
