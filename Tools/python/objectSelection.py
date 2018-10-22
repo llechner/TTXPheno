cd561eb59d46f2889728092ad79d4ee45757695a
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2018-10-22 13:49:01 +0200

    small & transparent

--- Tools/python/objectSelection.py
+++ Tools/python/objectSelection.py
@@ -1,6 +1,8 @@
 ''' ObjectSelections
 '''
 
+max_lepton_abseta = 3.0
+max_jet_abseta = 4.0
 
 def genFraction( fwlite_jet, pdgId ):
     return sum( [ fwlite_jet.getGenConstituent(i).pt() for i in range(fwlite_jet.numberOfSourceCandidatePtrs()) if abs(fwlite_jet.getGenConstituent(i).pdgId()) == pdgId ], 0 )/fwlite_jet.pt()
@@ -11,29 +13,29 @@ def genJetId( fwlite_jet ):
 def isGoodGenJet( j ):
     ''' jet object selection
     '''
-    return j['pt'] > 30 and abs( j['eta'] ) < 4.0 #2.4
+    return j['pt'] > 30 and abs( j['eta'] ) < max_jet_abseta
 
 def isGoodGenPhoton( j ):
-    ''' jet object selection
+    ''' photon object selection
     '''
     return j['pt'] > 15 and abs( j['eta'] ) < 2.1
 
 def isGoodGenLepton( l ):
     ''' lepton object selection
     '''
-    return l['pt'] > 10 and abs( l['eta'] ) < 3.0 and abs( int(l['pdgId']) ) in [11,13] #eta < 2.5
+    return l['pt'] > 10 and abs( l['eta'] ) < max_lepton_abseta and abs( int(l['pdgId']) ) in [11,13] #eta < 2.5
 
 def isGoodRecoLepton( l ):
-    return l['pt'] > 10 and abs( l['eta'] ) < 3.0 and abs( int(l['pdgId']) ) in [11,13] #eta < 2.5
+    return l['pt'] > 10 and abs( l['eta'] ) < max_lepton_abseta and abs( int(l['pdgId']) ) in [11,13] #eta < 2.5
 
 def isGoodRecoMuon( l ):
-    return abs( l['pdgId'] ) == 13 and abs( l['eta'] ) < 3.0 and l['pt'] > 10 #eta < 2.5
+    return abs( l['pdgId'] ) == 13 and abs( l['eta'] ) < max_lepton_abseta and l['pt'] > 10 #eta < 2.5
 
 def isGoodRecoElectron( l ):
-    return abs( l['pdgId'] ) == 11 and abs( l['eta'] ) < 3.0 and l['pt'] > 10 #eta < 2.5
+    return abs( l['pdgId'] ) == 11 and abs( l['eta'] ) < max_lepton_abseta and l['pt'] > 10 #eta < 2.5
 
 def isGoodRecoJet( j, pt_var = 'pt'):
-    return  abs( j['eta'] ) < 4.0 and j[pt_var] > 30 and j['nCharged']>1 and j['nNeutrals']>0 #eta < 2.4
+    return  abs( j['eta'] ) < max_jet_abseta and j[pt_var] > 30 and j['nCharged']>1 and j['nNeutrals']>0 #eta < 2.4
 
 def isGoodRecoPhoton( g ):
     return  abs( g['eta'] ) < 2.1 and g['pt'] > 15
